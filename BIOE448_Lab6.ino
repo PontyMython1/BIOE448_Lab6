7e38472813a47c5ccdce822409df4a2a839bcb0e
PontyMython1 <mblitz03@gmail.com>
2025-02-26 14:23:02 -0600

    threshold refinement

--- BIOE448_Lab6.ino
+++ BIOE448_Lab6.ino
@@ -6,7 +6,7 @@ bool first_peak_detected = false;
 unsigned long first_pulse_time = 0;
 unsigned long second_pulse_time = 0;
 int upper_threshold = 860;
-int lower_threshold = 760;
+int lower_threshold = 765;
 float pulse_period = 0;
 void setup() {
   Serial.begin(9600);
@@ -15,7 +15,7 @@ void setup() {
 
 void loop() {
   pulse_signal = analogRead(sensor_pin);
-  //Serial.println(pulse_signal);
+  Serial.println(pulse_signal);
   delay(100);
   if (pulse_signal > upper_threshold && any_peak_detected == false) {
     any_peak_detected = true;
@@ -28,7 +28,7 @@ void loop() {
            first_peak_detected = false;
            any_peak_detected = false;
            BPM = (60000/(pulse_period)); 
-           Serial.println(BPM);
+           //Serial.println(BPM);
         }
     
 }
